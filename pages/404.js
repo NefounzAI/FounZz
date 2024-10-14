681c184c6b7f0eb7eaef82e90c10d63c0f7f6123
Rifqiii <rifqibkl14@gmail.com>
2024-10-14 11:15:25 +0700

    Ubah route a menjadi link

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 /* eslint-disable react/no-unescaped-entities */
 export default function Custom404() {
   return (
@@ -6,12 +8,12 @@ export default function Custom404() {
         <h1 className="text-6xl font-bold mb-4 animate-bounce">404</h1>
         <p className="text-2xl mb-6">Page Not Found</p>
         <p className="mb-8">Oops! It seems like the page you're looking for doesn't exist.</p>
-        <a
+        <Link
           href="/"
           className="inline-block px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg transition-colors"
         >
           Go Back Home
-        </a>
+          </Link>
       </div>
     </div>
   );
